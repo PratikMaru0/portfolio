ebd62ce8c06c254515def30a79d64243dca4c4e9
Pratik maru <pratikmaru80762@example.com>
2025-05-17 22:19:42 +0530

    update static text usage

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "./common";
 import Input from "./common/Input";
 import TextArea from "./common/TextArea";
+import { contactTxt } from "../constants/texts";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -12,36 +13,35 @@ const Contact = () => {
     <section className="flex flex-1 items-center justify-center">
       <div className="max-w-2xl w-full text-center space-y-6">
         <div className="space-y-2">
-          <p className="mt-8 text-lg">Connect with me</p>
-          <h1 className="text-5xl font-serif font-semibold">Get in touch</h1>
-          <p className="text-base mt-2">
-            I&apos;d love to hear from you! If you have any questions, comments
-            or feedback, please use the form below.
-          </p>
+          <p className="mt-8 text-lg">{contactTxt.subtitle}</p>
+          <h1 className="text-5xl font-serif font-semibold">
+            {contactTxt.title}
+          </h1>
+          <p className="text-base mt-2">{contactTxt.description}</p>
         </div>
         <form className="mt-8 space-y-6">
           <div className="flex flex-col md:flex-row gap-4">
             <Input
               type="text"
-              placeholder="Enter your name"
+              placeholder={contactTxt.namePlaceholder}
               val={name}
               setVal={setName}
             />
             <Input
               type="email"
-              placeholder="Enter your email"
+              placeholder={contactTxt.emailPlaceholder}
               val={email}
               setVal={setEmail}
             />
           </div>
 
           <TextArea
-            placeholder="Enter your message"
+            placeholder={contactTxt.messagePlaceholder}
             val={message}
             setVal={setMessage}
           />
           <div className="flex justify-center">
-            <Button text="Submit Now -> " />
+            <Button text={contactTxt.submitButton} />
           </div>
         </form>
       </div>
--- src/constants/texts/contactTxt.ts
+++ src/constants/texts/contactTxt.ts
@@ -1,4 +1,12 @@
 const contactTxt = {
+  title: "Get in touch",
+  subtitle: "Connect with me",
+  description:
+    "I'd love to hear from you! If you have any questions, comments or feedback, please use the form below.",
+  namePlaceholder: "Enter your name",
+  emailPlaceholder: "Enter your email",
+  messagePlaceholder: "Enter your message",
+  submitButton: "Submit Now -> ",
 };
 
 export default contactTxt;
